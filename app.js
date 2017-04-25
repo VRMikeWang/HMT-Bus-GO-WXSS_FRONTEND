bcb26a7528d764ff7c09732a10c32c9a008e93de
CRH380A-2722 <609657831@qq.com>
2017-04-26 02:04:28 +0800

    小程序逻辑

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ *  HMT Bus GO! (WXSS VER.)
+ *
+ *  @author CRH380A-2722 <609657831@qq.com>
+ *  @copyright 2016-2017 CRH380A-2722
+ *  @license MIT
+ *	@note 小程序逻辑
+ */
+
+/* 注册小程序 */
+
+App({
+
+	onLaunch: function(options) {
+		
+	},
+
+	onShow: function(options) {
+		// Do something when show.
+	},
+
+	onHide: function() {
+		// Do something when hide.
+	},
+
+	onError: function(msg) {
+		console.log(msg);
+	},
+
+	//globalData: 'I am global data'
+
+});
